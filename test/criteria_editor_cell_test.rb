7bdc4c8ca5ad9f6834abd467ab412ac35361b7cf
Florian Koch <fkoch@selectline.de>
2017-05-22 15:10:23 +0200

    passed valid objects to test

--- test/criteria_editor_cell_test.rb
+++ test/criteria_editor_cell_test.rb
@@ -3,12 +3,12 @@ require_relative 'test_helper'
 class CriteriaEditorCellTest < Cell::TestCase
 
   def test_direct_instantiation_html
-    html = CriteriaOperator::UiComponent::CriteriaEditorCell.(42).()
+    html = CriteriaOperator::UiComponent::CriteriaEditorCell.(CriteriaOperator::GroupOperator.new).()
     assert !html.nil?
   end
 
   def test_helper_html
-    html = cell("criteria_operator/ui_component/criteria_editor", 42).()
+    html = cell("criteria_operator/ui_component/criteria_editor", CriteriaOperator::GroupOperator.new).()
     assert !html.nil?
   end
 end
